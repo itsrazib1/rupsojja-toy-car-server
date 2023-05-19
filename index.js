94387079fdd93b63fdb39b1d3b35251b33e09da7
itsrazib1 <itsrazib1@gmail.com>
2023-05-19 11:54:24 +0600

    server update

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const cors = require('cors');
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 require('dotenv').config()
 const app = express();
 const port = process.env.PORT || 5000;
@@ -28,12 +28,34 @@ async function run() {
   try {
     // Connect the client to the server	(optional starting in v4.7)
     await client.connect();
+
+const carCollection = client.db('carZone').collection('cars');
+
+app.get('/cars', async(req , res) => {
+    const carsor = carCollection.find();
+    const result = await carsor.toArray();
+
+    res.send(result);
+})
+
+app.get('/cars/:id', async(req , res) => {
+    const id = req.params.id;
+    const query = {_id: new ObjectId(id) }
+
+const options = {
+    projection: {title: 1 ,price: 1 , service_id: 1,img: 1},
+};
+
+    const result = await carCollection.findOne(query,options );
+    res.send(result);
+})
+
     // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
     console.log("Pinged your deployment. You successfully connected to MongoDB!");
   } finally {
     // Ensures that the client will close when you finish/error
-    await client.close();
+    // await client.close();
   }
 }
 run().catch(console.dir);
