3606878f68bf848e9aa8405de4b1a3fb43a0794c
itsrazib1 <itsrazib1@gmail.com>
2023-05-21 18:39:19 +0600

    addef Modifay

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
-const express = require('express');
-const cors = require('cors');
-const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
-require('dotenv').config()
+const express = require("express");
+const cors = require("cors");
+const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
+require("dotenv").config();
 const app = express();
 const port = process.env.PORT || 5000;
 
@@ -9,8 +9,6 @@ const port = process.env.PORT || 5000;
 
 app.use(cors());
 app.use(express.json());
-
-
 
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.4hmio3i.mongodb.net/?retryWrites=true&w=majority`;
 
@@ -20,7 +18,7 @@ const client = new MongoClient(uri, {
     version: ServerApiVersion.v1,
     strict: true,
     deprecationErrors: true,
-  }
+  },
 });
 
 async function run() {
@@ -28,68 +26,110 @@ async function run() {
     // Connect the client to the server	(optional starting in v4.7)
     await client.connect();
 
-const carCollection = client.db('carZone').collection('cars');
-const cartCollection = client.db('carZone').collection('cart');
+    const carCollection = client.db("carZone").collection("cars");
+    const cartCollection = client.db("carZone").collection("cart");
 
-app.get('/cars', async(req , res) => {
-    const carsor = carCollection.find();
-    const result = await carsor.toArray();
+    app.get("/cars", async (req, res) => {
+      const carsor = carCollection.find();
+      const result = await carsor.toArray();
 
-    res.send(result);
-})
+      res.send(result);
+    });
 
-app.get('/cars/:id', async(req , res) => {
-    const id = req.params.id;
-    const query = {_id: new ObjectId(id) }
+    app.get("/cars/:id", async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: new ObjectId(id) };
 
-const options = {
-    projection: {name: 1 ,Price: 1 ,_id: 1,url: 1,Sellername: 1,Subcategory: 1,quantity: 1,Rating: 1,description:1,},
-};
+      const options = {
+        projection: {
+          name: 1,
+          Price: 1,
+          _id: 1,
+          url: 1,
+          Sellername: 1,
+          Subcategory: 1,
+          quantity: 1,
+          Rating: 1,
+          description: 1,
+        },
+      };
 
-    const result = await carCollection.findOne(query,options );
-    res.send(result);
-})
+      const result = await carCollection.findOne(query, options);
+      res.send(result);
+    });
 
+    app.get("/cars/:id", async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: new ObjectId(id) };
+      const result = await carCollection.findOne(query);
+      res.send(result);
+    });
 
-// booking
+    app.put("/cars/:id", async (req, res) => {
+      const id = req.params.id;
+      const filter = { _id: new ObjectId(id) };
+      const option = { upsert: true };
+      const updatedCar = req.body;
+      const Car = {
+        $set: {
+          name: updatedCar.name,
+          url: updatedCar.url,
+          Sellername: updatedCar.Sellername,
+          Selleremail: updatedCar.Selleremail,
+          Subcategory: updatedCar.Subcategory,
+          Price: updatedCar.Price,
+          quantity: updatedCar.quantity,
+          Rating: updatedCar.Rating,
+          description: updatedCar.description,
+        }
+      }
+      const Ruselt = await carCollection.updateOne(filter,Car,option)
+      res.send(Ruselt)
+    });
+    // booking
 
-app.get('/cart', async(req, res) => {
-let query = {};
-if(req.query?.email){
-    query = {email: req.query.email}
-}
-    const result = await cartCollection.find(query).toArray();
-    res.send(result);
-  });
-  
-app.post('/toys',async (req,res)=>{
-  const newToys = req.body;
-  console.log(newToys);
-  const result = await carCollection.insertOne(newToys);
-  res.send(result);
+    app.get("/cart", async (req, res) => {
+      let query = {};
+      if (req.query?.email) {
+        query = { email: req.query.email };
+      }
+      const result = await cartCollection.find(query).toArray();
+      res.send(result);
+    });
 
-})
+    app.post("/toys", async (req, res) => {
+      const newToys = req.body;
+      console.log(newToys);
+      const result = await carCollection.insertOne(newToys);
+      res.send(result);
+    });
 
+    app.post("/cart", async (req, res) => {
+      const cart = req.body;
+      console.log(cart);
+      const result = await cartCollection.insertOne(cart);
+      res.send(result);
+    });
 
+    app.delete("/cart/:id", async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: new ObjectId(id) };
+      const result = await cartCollection.deleteOne(query);
+      res.send(result);
+    });
 
-
-app.post('/cart',async (req,res) => {
-    const cart = req.body;
-    console.log(cart);
-    const result = await cartCollection.insertOne(cart);
-    res.send(result);
-});
-
-app.delete('/cart/:id',async (req,res)=>{
-  const id = req.params.id;
-  const query = {_id: new ObjectId(id)}
-  const result = await cartCollection.deleteOne(query);
-  res.send(result);
-})
+    app.delete("/cars/:id", async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: new ObjectId(id) };
+      const result = await carCollection.deleteOne(query);
+      res.send(result);
+    });
 
     // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
-    console.log("Pinged your deployment. You successfully connected to MongoDB!");
+    console.log(
+      "Pinged your deployment. You successfully connected to MongoDB!"
+    );
   } finally {
     // Ensures that the client will close when you finish/error
     // await client.close();
@@ -97,12 +137,10 @@ app.delete('/cart/:id',async (req,res)=>{
 }
 run().catch(console.dir);
 
+app.get("/", (req, res) => {
+  res.send("RTCS Is Running");
+});
 
-
-app.get('/' , (req,res) => {
-    res.send('RTCS Is Running')
-})
-
-app.listen(port , () => {
-    console.log(`RTCS is Running on port ${port} `);
-})
+app.listen(port, () => {
+  console.log(`RTCS is Running on port ${port} `);
+});
